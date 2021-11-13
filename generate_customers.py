a83a5a4918958b366eea1457ff3b7161ef9a8b54
mosc5 <moritz.schiel@gmx.net>
2021-11-13 14:39:40 +0100

    write generate script #1

--- generate_customers.py
+++ generate_customers.py
@@ -8,4 +8,32 @@ Date: 13.11.2021
 
 """
 
+import random
+import datetime
+import json
 
+
+def generate_customers(amount: int, random_seed: int, input, output):
+    random.seed(random_seed)
+    customer_list = []
+    loc_list = []
+    with open('data/' + input) as f:
+        data = json.load(f)
+    for point in data:
+        point['name']
+    for counter in range(amount):
+        time = datetime.time(hour=random.randrange(24), minute=random.randrange(60), second=random.randrange(60))
+        name = "customer" + str(counter)
+        start = random.choice(loc_list)
+        destination = random.choice(loc_list)
+        while start == destination:
+            destination = random.choice(loc_list)
+        customer_dict = {
+            'name': name,
+            'starting_time': time,
+            'starting_point': start,
+            'destination': destination
+        }
+        customer_list.append(customer_dict)
+    with open('data/' + output, 'w') as json_file:
+        json.dump(customer_list, json_file)
