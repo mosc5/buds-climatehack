6ae712ef1f4fc0035a2aec307fbdec5de778a325
mosc5 <moritz.schiel@gmx.net>
2021-11-13 14:48:28 +0100

    make generate script runnable #1

--- generate_customers.py
+++ generate_customers.py
@@ -13,14 +13,14 @@ import datetime
 import json
 
 
-def generate_customers(amount: int, random_seed: int, input, output):
+def generate_customers(amount: int, random_seed: int, input_file, output_file):
     random.seed(random_seed)
     customer_list = []
     loc_list = []
-    with open('data/' + input) as f:
+    with open('data/' + input_file) as f:
         data = json.load(f)
     for point in data:
-        point['name']
+        loc_list.append(point['name'])
     for counter in range(amount):
         time = datetime.time(hour=random.randrange(24), minute=random.randrange(60), second=random.randrange(60))
         name = "customer" + str(counter)
@@ -30,10 +30,14 @@ def generate_customers(amount: int, random_seed: int, input, output):
             destination = random.choice(loc_list)
         customer_dict = {
             'name': name,
-            'starting_time': time,
+            'starting_time': str(time),
             'starting_point': start,
             'destination': destination
         }
         customer_list.append(customer_dict)
-    with open('data/' + output, 'w') as json_file:
+    with open('data/' + output_file, 'w') as json_file:
         json.dump(customer_list, json_file)
+
+
+if __name__ == '__main__':
+    generate_customers(5, 0, 'points.json', 'customers.json')
